a272b4c23a52d0f7f3b9bd57f65d9b0fc7b19ccb
michal.kosek <michal.kosek@upp.cz>
2025-10-14 16:36:07 +0200

    papyrus colorscheme

--- ColorScheme.cpp
+++ ColorScheme.cpp
@@ -8,6 +8,7 @@ const std::unordered_map<QString, ColorScheme::T, hash_qstring> ColorScheme::sch
 	,{ "Peach", { "#5a5255", "#fae0ad", "#e1c99b", "#7a7476"} }
 	,{ "Sprout", { "#5a5255", "#c6d7b2", "#b2c1a0", "#7a7476"} }
 	,{ "Tropical Blue", { "#5a5255", "#c4def0", "#b0c7d8", "#7a7476"} }
+	,{ "Papyrus", { "#0b5369", "#f4edd6", "#f4edd6", "#7a7476"} }
 	,{ "Dark", { "#dddddd", "#5a5255", "#6a6366", "#bbbbbb"} }
 };
 
